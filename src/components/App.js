0f504b99396ed88971770f7ea749c48d9fe31f68
4hmaad <51507194+4hmaad@users.noreply.github.com>
2020-04-23 19:52:36 +0500

    removed console log

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,6 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID)
     return (
       <div className="ui container">
         <BrowserRouter>
