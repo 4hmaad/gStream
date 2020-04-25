3518ef0a5aebfaefbbc766652f44675e2e10ebb4
4hmaad <51507194+4hmaad@users.noreply.github.com>
2020-04-26 03:31:53 +0500

    fixed streamReducer function

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,7 +1,5 @@
 export default (state = [], action) => {
-  if (action.type === "CREATE_STREAM") {
-    return [...state, action.payload]
-  } else if (action.type === "FETCH_STREAMS") {
+  if (action.type === "FETCH_STREAMS") {
     return action.payload
   }
 
