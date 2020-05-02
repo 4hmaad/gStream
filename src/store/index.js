bee55582aae75733c788de75660a87e2234c8d9d
4hmaad <51507194+4hmaad@users.noreply.github.com>
2020-05-02 22:14:58 +0500

    fixed paths in store file

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware, compose } from "redux"
-import reducers from "./reducers"
 import thunk from "redux-thunk"
+
+import reducers from "../reducers"
+
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
