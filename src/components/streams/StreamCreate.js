cf459671980e557a93a04d15397710877a612a96
4hmaad <51507194+4hmaad@users.noreply.github.com>
2020-05-02 23:47:47 +0500

    added alert after stream submit

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,19 +1,27 @@
-import React from "react"
-import { reduxForm, Field } from "redux-form"
-import { connect } from "react-redux"
+import React from "react";
+import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
 
 /* Configs */
+import { miniAlert, alert } from "../../configs/SweetAlertConfig";
 /* Elements */
-import { Label, Form, Button, Header as HeaderEl, Segment, Input } from "semantic-ui-react"
+import {
+  Label,
+  Form,
+  Button,
+  Header as HeaderEl,
+  Segment,
+  Input,
+} from "semantic-ui-react";
 
 /* Action Creators */
-import { createStream } from "../../actions/index"
+import { createStream } from "../../actions/index";
 
 class StreamCreate extends React.Component {
   constructor(props) {
-    super(props)
-    this.renderInput = this.renderInput.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
+    super(props);
+    this.renderInput = this.renderInput.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   renderError({ error, touched }) {
@@ -22,7 +30,7 @@ class StreamCreate extends React.Component {
         <Label basic color="red" pointing>
           {error}
         </Label>
-      )
+      );
     }
   }
 
@@ -33,55 +41,67 @@ class StreamCreate extends React.Component {
         <Input {...input} />
         {this.renderError(meta)}
       </Form.Field>
-    )
+    );
   }
 
   onSubmit(values) {
-    return this.props.createStream(values)
+    return this.props.createStream(values).then(() => {
+      miniAlert.fire({
+        icon: "success",
+        title: "Stream Created Successfully",
+      });
+    });
   }
 
   render() {
-    const { submitting, invalid } = this.props
+    const { submitting, invalid } = this.props;
     return (
       <Segment size="large" color="green">
         <Form onSubmit={this.props.handleSubmit(this.onSubmit)}>
           <HeaderEl as="h3" style={{ marginBottom: "2rem" }}>
             Create a New Stream
           </HeaderEl>
-          <Field name="title" component={this.renderInput} label="Stream Title" />
-          <Field name="description" component={this.renderInput} label="Stream Description" />
+          <Field
+            name="title"
+            component={this.renderInput}
+            label="Stream Title"
+          />
+          <Field
+            name="description"
+            component={this.renderInput}
+            label="Stream Description"
+          />
           <Button primary disabled={submitting || invalid}>
-        Submit
-      </Button>
-          
+            Submit
+          </Button>
         </Form>
       </Segment>
-    )
+    );
   }
 }
 
-const validate = values => {
-  const errors = {}
+const validate = (values) => {
+  const errors = {};
 
   if (!values.title) {
-    errors.title = "Please enter the title"
+    errors.title = "Please enter the title";
   }
   if (!values.description) {
-    errors.description = "Please enter the description"
+    errors.description = "Please enter the description";
   }
 
-  return errors
-}
+  return errors;
+};
 
-const mapStateToProps = state => {
-  return state
-}
+const mapStateToProps = (state) => {
+  return state;
+};
 
 const formWrapped = reduxForm({
   form: "streamCreate",
-  validate
-})(StreamCreate)
+  validate,
+})(StreamCreate);
 
 export default connect(mapStateToProps, {
-  createStream
-})(formWrapped)
+  createStream,
+})(formWrapped);
