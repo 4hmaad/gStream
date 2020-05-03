abde2e31bb57af1d34c148bb137ae8eda429a098
4hmaad <51507194+4hmaad@users.noreply.github.com>
2020-05-03 20:50:57 +0500

    added loader and error

--- src/components/streams/StreamPage.js
+++ src/components/streams/StreamPage.js
@@ -10,36 +10,62 @@ import {
   Loader,
   Dimmer,
   Divider,
-  Item,
 } from "semantic-ui-react";
 
 class StreamPage extends React.Component {
-  render() {
-    if (this.props.stream) {
-      return (
-        <Fragment>
-          <Grid.Column width="16">
-            <Segment size="small" color="blue">
-              <HeaderEl size="huge"> {this.props.stream.title} </HeaderEl>
-              <Embed placeholder="/images/image-16by9.png" source="url" />
-              <Divider />
+  renderStream() {
+    return (
+      <Fragment>
+        <HeaderEl size="huge"> {this.props.stream.title} </HeaderEl>
+        <Embed placeholder="/images/image-16by9.png" source="url" />
+        <Divider />
 
-              <HeaderEl>Description</HeaderEl>
-              <p>{this.props.stream.description}</p>
-            </Segment>
-          </Grid.Column>
-        </Fragment>
-      );
+        <HeaderEl>Description</HeaderEl>
+        <p>{this.props.stream.description}</p>
+      </Fragment>
+    );
+  }
+
+  renderError() {
+    return <HeaderEl as="h3"> Stream deleted or doesn't exist </HeaderEl>;
+  }
+
+  renderLoader() {
+    return (
+      <Dimmer active inverted>
+        <Loader />
+      </Dimmer>
+    );
+  }
+
+  renderContent = () => {
+    if (this.props.stream.isLoading === false && this.props.stream.id) {
+      return this.renderStream();
+    } else if (this.props.stream.isLoading === false && !this.props.stream.id) {
+      return this.renderError();
     }
 
-    return "Loading...";
+    return this.renderLoader();
+  };
+
+  render() {
+    return (
+      <Grid.Column width="16">
+        <Segment size="small" color="blue">
+          {this.renderContent()}
+        </Segment>
+      </Grid.Column>
+    );
   }
 }
 
 const mapStateToProps = ({ streams }, { match }) => {
-  const stream = streams.find((stream) => stream.id === match.params.id);
+  if (streams.length) {
+    const stream = streams.find((stream) => stream.id === match.params.id);
+    return { stream: { ...stream, isLoading: false } };
+  }
 
-  return { stream };
+  return { stream: { isLoading: true } };
 };
 
 export default connect(mapStateToProps)(StreamPage);
