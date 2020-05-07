6b8398d23a09ab5cfa881920eb07fd1d3223c593
4hmaad <51507194+4hmaad@users.noreply.github.com>
2020-05-08 03:05:56 +0500

    change combineReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
 import streamReducer from "./streamReducer";
+import userReducer from "./userReducer";
 
 export default combineReducers({
   auth: authReducer,
   form: formReducer,
-  streams: streamReducer,
+  stream: streamReducer,
+  users: userReducer,
 });
